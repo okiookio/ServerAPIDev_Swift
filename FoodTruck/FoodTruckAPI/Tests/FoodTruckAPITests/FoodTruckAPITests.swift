488ac4c936135fa231c35e4ae657f108c0443337
Tim Beals <tim.beals@gmail.com>
2017-05-09 10:17:58 -0700

    Finish initial project setup with detailed notes.

---
+++ FoodTruck/FoodTruckAPI/Tests/FoodTruckAPITests/FoodTruckAPITests.swift
@@ -0,0 +1,17 @@
+import XCTest
+@testable import FoodTruckAPI
+
+class FoodTruckAPITests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(FoodTruckAPI().text, "Hello, World!")
+    }
+
+
+    static var allTests : [(String, (FoodTruckAPITests) -> () throws -> Void)] {
+        return [
+            ("testExample", testExample),
+        ]
+    }
+}
