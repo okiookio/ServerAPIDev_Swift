2ad11f9a1a2a9f845e41b138856e45767251509d
Tim Beals <tim.beals@gmail.com>
2017-05-10 13:30:48 -0700

    create food truck model with extensions to create JSON dictionaries

--- FoodTruck/FoodTruckAPI/Sources/FoodTruckAPI/FoodTruckItem.swift
+++ FoodTruck/FoodTruckAPI/Sources/FoodTruckAPI/FoodTruckItem.swift
@@ -35,8 +35,38 @@ public struct FoodTruckItem {
     
 }
 
-//Equatable is a protocol that means that you are creating a type that can  be measured for equality.
+//Equatable is a protocol that means that you are creating a type that can  be measured for equality. Here we are overwriting the binary operator == so that when we put a foodtruckitem on either side, we can test all of their parameters against each other with a single operation.
 
 extension FoodTruckItem: Equatable {
+    public static func == (lhs: FoodTruckItem, rhs: FoodTruckItem) -> Bool {
+        return lhs.docId == rhs.docId &&
+        lhs.name == rhs.name &&
+        lhs.foodType == rhs.foodType &&
+        lhs.avgCost == rhs.avgCost &&
+        lhs.latitude == rhs.latitude &&
+        lhs.longitude == rhs.longitude
+    }
+}
+
+//note that all of the keys use lowercase. This is the convention for databases.
+extension FoodTruckItem: DictionaryConvertible {
     
+    internal func toDict() -> JSONDictionary {
+        var result = JSONDictionary()
+        result["docId"] = self.docId
+        result["name"] = self.name
+        result["foodtype"] = self.foodType
+        result["avgcost"] = self.avgCost
+        result["latitude"] = self.latitude
+        result["longitude"] = self.longitude
+        return result
+    }
+}
+
+//If we have an array where the elements conform to our DictionaryConvertible protocol, we can use the same method but perform the toDic for every element of the array.
+
+extension Array where Element: DictionaryConvertible {
+    func toDict() -> [JSONDictionary] {
+        return self.map { $0.toDict()}
+    }
 }
